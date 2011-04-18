5f66de7054f8f6096b40027d2bd0243907e18213
Bryan Zarzuela <bryan@teleserv.ph>
2011-04-18 12:57:52 +0800

    starting point of the test cases

---
+++ tests/unit.php
@@ -0,0 +1,21 @@
+<?php 
+
+include_once 'bootstrap.php';
+
+class MailCustomizerTest extends PHPUnit_Framework_TestCase
+{
+  public function testCustomizeViaZendConfig()
+  {
+    
+  }
+  
+  public function testCustomizeViaArray()
+  {
+    
+  }
+  
+  public function testCustomizeExistingObject()
+  {
+    
+  }
+}
